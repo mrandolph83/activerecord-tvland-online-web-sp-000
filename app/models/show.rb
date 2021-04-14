cf312b72fbfd58b1f2a1b2ba99a308e5572b4820
mrandolph83 <mrandolph83@gmail.com>
2021-04-14 13:35:17 -0400

    commit tree aec78efdba1c77eabddddbde48c3201bd4ae956d

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,21 @@
+require 'pry'
 class Show < ActiveRecord::Base
+    has_many :characters
+    belongs_to :network
+    has_many :actors, through: :characters 
+
+    def actors_list
+        character_list = []
+        self.characters.each do |character|
+        character_list << character.actor 
+        # binding.pry
+        end
+        full_names = []
+        character_list.each do |actor|
+        full_names << "#{actor.first_name} #{actor.last_name}"
+        end
+        full_names
+    end
   
+
 end
